8946c1c951ece4823e312eadeac5403dab95a6fd
Rafael4001 <rafal.ciesielczuk@gmail.com>
2018-01-13 00:50:20 +0100

    gulp + sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 
-gulp.task('hello', function () {
-  console.log("hello");
+gulp.task('sass', function () {
+  return gulp.src('app/scss/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
 });
